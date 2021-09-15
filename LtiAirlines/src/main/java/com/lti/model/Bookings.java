3b6deafad7401aa04ced8a1d2274d40677dc6e2a
vikas reddy <vikas reddy@DESKTOP-96I94D6>
2021-09-15 12:14:33 +0530

    second

--- LtiAirlines/src/main/java/com/lti/model/Bookings.java
+++ LtiAirlines/src/main/java/com/lti/model/Bookings.java
@@ -1,5 +1,18 @@
 package com.lti.model;
 
 public class Bookings {
-
+	int bookingNo;
+	Flight flight;
+	public int getBookingNo() {
+		return bookingNo;
+	}
+	public void setBookingNo(int bookingNo) {
+		this.bookingNo = bookingNo;
+	}
+	public Flight getFlight() {
+		return flight;
+	}
+	public void setFlight(Flight flight) {
+		this.flight = flight;
+	}
 }
